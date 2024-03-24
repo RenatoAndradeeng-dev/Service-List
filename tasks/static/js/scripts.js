388b97f027c969b5d07578a1fa9c35b36534e07b
RenatoAndradeeng-dev <renatinhoandrade98@gmail.com>
2024-03-23 21:40:38 -0300

    projeto finalizado

---
+++ tasks/static/js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    var deleteBtn = $('.delete-btn');
+    var searchBtn = $('.#search-btn');
+    var searchForm = $('.#search-form');
+
+    $(deleteBtn).on('click', function(e) {
+
+       e.preventDefault();
+
+       var delLink = $(this).attr('href');
+       var result = confirm('Deseja realmente deletar essa consulta?');
+
+       if(result) {
+          window.location.href = delLink;
+       }
+
+    });
+});
+
+$(searchBtn).on('click', function() {
+    searchForm.submit();
+
+});
+
+
